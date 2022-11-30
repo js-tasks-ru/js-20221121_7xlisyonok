bc9e5fee673c5123a2364ec6c32e0dc9568c4ce8
7xLisyonok <7x.Lisyonok@gmail.com>
2022-11-30 15:32:24 +1000

    trim symbols

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -4,6 +4,29 @@
  * @param {number} size - the allowed size of consecutive identical symbols
  * @returns {string} - the new string without extra symbols according passed size
  */
-export function trimSymbols(string, size) {
+export function trimSymbols(string, size = Infinity) {
+  const chars = string.split("");
 
+  const resultDefaults = {
+    chars: [], // Символы  новой строки
+    index: 0, // Количество повторений
+    prevChar: null, // Предыдущий символ
+  };
+
+  const result = chars.reduce((result, char) => {
+    if (char !== result.prevChar) {
+      result.prevChar = char;
+      result.index = 0;
+    } else {
+      result.index++;
+    }
+
+    if (result.index < size) {
+      result.chars.push(char);
+    }
+
+    return result;
+  }, resultDefaults);
+
+  return result.chars.join("");
 }
