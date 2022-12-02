73b4f339d6688dcf637cf3b2d6f7f48b888aa58c
7xLisyonok <7x.Lisyonok@gmail.com>
2022-12-02 15:19:41 +1000

    short solve for uniq

--- 03-objects-arrays-intro-to-testing/4-uniq/index.js
+++ 03-objects-arrays-intro-to-testing/4-uniq/index.js
@@ -3,10 +3,4 @@
  * @param {*[]} arr - the array of primitive values
  * @returns {*[]} - the new array with uniq values
  */
-export function uniq(arr) {
-  if (!Array.isArray(arr)) return [];
-
-  const uniques = new Set();
-  arr.forEach((e) => uniques.add(e));
-  return [...uniques];
-}
+export const uniq = (arr) => [...new Set(arr)];
